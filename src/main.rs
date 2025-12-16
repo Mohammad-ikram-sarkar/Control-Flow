471b21811435f7fa797905333bc50367f0a0a3ed
Mohammad-ikram-sarkar <islamikram357@gmail.com>
2025-12-16 21:12:19 +0600

    Using if in a let Statement

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,32 @@ fn main() {
 // this i only number  use  error me.  example 
 // if number { println("example ")} this is not work 
 
-let number  = number();
-if number % 4 == 0 {
-            println!("number is divisible by 4");
+// let number  = number();
+// if number % 4 == 0 {
+//             println!("number is divisible by 4");
 
-}else if number % 3 == 0 {
-    println!("number divisible by 3");
+// }else if number % 3 == 0 {
+//     println!("number divisible by 3");
 
 
-} else if  number % 2 == 0 {
-    println!("number divisible by 2") ;
+// } else if  number % 2 == 0 {
+//     println!("number divisible by 2") ;
+// }
+// println!("number is : {} " , number);
+
+
+// using if in a let statemant
+
+let condition = false ;
+let number = if  condition {5} else {6};
+println!("The value of number is: {number}");
+
+
+
+
 }
-println!("number is : {} " , number);
 
 
-
-
-}
-
-
-fn number() -> i32 {
-    20
-}
+// fn number() -> i32 {
+//     20
+// }
